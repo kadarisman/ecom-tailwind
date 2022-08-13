057af2137aa76486ee35bf6ea663dd213224a546
kadarisman <yuzarqadariesman@gmail.com>
2022-08-13 14:07:43 +0700

    rumit manage state pakai props

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
   
   useEffect(() => {
     getProducts();
+    
   },[]);
 
   const getProducts = async ()=>{
@@ -24,25 +25,26 @@ const Home = () => {
     const response = await axios.get('https://fakestoreapi.com/products/');
     const productByCategory = response.data.filter((item, index ) => item.category === categorySelected);
     setProduct(productByCategory);
-    setProductFilter(productByCategory);
+    //setProductFilter(productByCategory);
   }
 
   const handleOrderBy = (e) => {
     const orderByValue = e;
-    // const produtcOrderbyPrice = productsFilter.sort((a, b) => (a.price > b.price ? 1 : -1));'oo'cc'dd.s.iqdd
+    // const produtcOrderbyPrice = productsFilter.sort((a, b) => (a.price > b.price ? dd1 : -1));'oo'dcccc'ddcc.c.XddeeXcc
     let produtcOrderbyPrice = [];
     if(orderByValue == 'murah'){
       console.log(e);
-      produtcOrderbyPrice = productsFilter.sort((a, b) => (a.price > b.price ? 1 : -1));
-      console.log(produtcOrderbyPrice);
+      const produtcOrderbyPriceMurah = products.sort((a, b) => (a.price > b.price ? 1 : -1));
+      console.log(produtcOrderbyPriceMurah);
+      //onsole.log(products);
       //setProduct(produtcOrderbyPrice);
     } else {
       console.log(e);
-      produtcOrderbyPrice = productsFilter.sort((a, b) => (a.price > b.price ? -1 : 1));
-      console.log(produtcOrderbyPrice);
+      const produtcOrderbyPriceMahal = products.sort((a, b) => (a.price > b.price ? -1 : 1));
+      console.log(produtcOrderbyPriceMahal);
       //setProduct(produtcOrderbyPrice);
     }
-    setProduct(produtcOrderbyPrice);
+    //setProduct([]);
     // console.log(produtcOrderbyPrice);
   }
 
